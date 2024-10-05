330e5a10ae04e1115db92ec9bc911d74d920240a
arafat-jahan <ajh.official15@gmail.com>
2024-10-05 13:12:59 +0600

    Added functionality to send notifications with title, text, and image URL using Firebase Cloud Messaging

---
+++ app/src/main/java/com/example/fcmnotification/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.fcmnotification
+
+import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.InputBinding
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fcmnotification.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    var dataString:String = ""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        if (intent != null) {
+            if (intent.hasExtra("key1")) {
+                for (key in intent.extras!!.keySet()) {
+                    dataString =  dataString + intent.extras!!.getString(key)+"\n"
+                    Log.d("myTag",key+intent.extras!!.getString(key))
+
+                }
+                binding.textView2.text = dataString
+
+
+            }
+
+        }
+    }
+}
